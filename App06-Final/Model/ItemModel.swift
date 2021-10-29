f05daed02b3b87702f44849920e9df7f37c59e2e
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2021-10-29 10:19:59 -0500

    made connection with api and loadItems

---
+++ App06-Final/Model/ItemModel.swift
@@ -0,0 +1,42 @@
+//
+//  ItemModel.swift
+//  App06-Final
+//
+//  Created by Clarissa Velásquez Magaña on 29/10/21.
+//
+
+import SwiftUI
+import Alamofire
+
+class ItemModel: ObservableObject {
+    @Published var items = [Item]()
+    
+    init() {
+        loadItems()
+    }
+    
+    func loadItems() {
+        let URL = "https://fakestoreapi.com/products"
+        
+        AF.request(URL, method: .get, encoding: URLEncoding.default).responseData { data in
+
+            let json = try! JSON(data: data.data!)
+            //print(json.count)
+            
+            var item: Item
+            for product in json {
+// //               print(movie.1["title"].stringValue)
+                item = Item(id: product.1["id"].intValue,
+                               name: product.1["title"].stringValue,
+                               price: product.1["price"].doubleValue,
+                               desc: product.1["description"].stringValue,
+                               category: product.1["category"].stringValue,
+                               image: product.1["image"].stringValue,
+                               rate: product.1["rating"]["rate"].doubleValue
+                )
+                self.items.append(item)
+            }
+        }
+    }
+}
+
