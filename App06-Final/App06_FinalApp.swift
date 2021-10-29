d50352ad4a94594468b56877a8d187146375efa6
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2021-10-29 09:08:30 -0500

    Initial Commit

---
+++ App06-Final/App06_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  App06_FinalApp.swift
+//  App06-Final
+//
+//  Created by Clarissa Velásquez Magaña on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct App06_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
